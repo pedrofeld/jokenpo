a4a5f716953a30a8c4761b40840a0cdaf9c2aad6
Pedro Gael Feld <pedrofeld@MacBook-Pro-de-Pedro.local>
2026-05-23 09:44:52 -0300

    feat: create functional game

---
+++ lib/game.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  var _appImage = AssetImage("images/default.png");
+  var _message = "Select an option below:";
+
+  void _selectOption(String userSelection){
+    var options = ["rock", "paper", "scissors"];
+    var number = Random().nextInt(3);
+    var appselection = options[number];
+
+    print("User selection: " + userSelection);
+    print("App selection: " + appselection);
+
+    switch(appselection){
+      case "rock":
+        setState(() {
+          this._appImage = AssetImage("images/rock.png");
+        });
+        break;
+      case "paper":
+        setState(() {
+          this._appImage = AssetImage("images/paper.png");
+        });
+        break;
+      case "scissors":
+        setState(() {
+          this._appImage = AssetImage("images/scissors.png");
+        });
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jokenpo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "App selection:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image(image: this._appImage,),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              _message,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => _selectOption("rock"),
+                child: Image.asset(
+                  "images/rock.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _selectOption("paper"),
+                child: Image.asset(
+                  "images/paper.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _selectOption("scissors"),
+                child: Image.asset(
+                  "images/scissors.png",
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
