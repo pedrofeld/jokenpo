d5714b02340f28cdcd3145bdffcd0eea71980834
Pedro Gael Feld <pedrofeld@MacBook-Pro-de-Pedro.local>
2026-05-24 09:46:51 -0300

    feat: add new pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:jokenpo/game.dart';
+import 'package:jokenpo/pages/home_page.dart';
+import 'package:jokenpo/pages/config_page.dart';
+import 'package:jokenpo/pages/game_page.dart';
+import 'package:jokenpo/pages/result_page.dart';
+import 'package:jokenpo/utils/app_colors.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      home: Game(),
+void main() {
+  runApp(const JokenpoApp());
+}
+
+class JokenpoApp extends StatelessWidget {
+  const JokenpoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jokenpô',
       debugShowCheckedModeBanner: false,
-    )
-  );
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: AppColors.background,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/config': (context) => const ConfigPage(),
+        '/game': (context) => const GamePage(),
+        '/result': (context) => const ResultPage(),
+      },
+    );
+  }
 }
